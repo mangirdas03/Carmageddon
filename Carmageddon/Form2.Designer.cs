f86ffc63263d1e3c4ee42345056cd18d5f021df3
Redas <redass95@outlook.com>
2022-10-15 01:09:37 +0300

    fixed - file in use error / no need 2 solution folders for testing

--- Carmageddon/Form2.Designer.cs
+++ Carmageddon/Form2.Designer.cs
@@ -62,7 +62,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(724, 9);
+            this.label1.Location = new System.Drawing.Point(750, 30);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(83, 20);
             this.label1.TabIndex = 0;
@@ -71,7 +71,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(745, 53);
+            this.label2.Location = new System.Drawing.Point(724, 65);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(0, 20);
             this.label2.TabIndex = 1;
