4747f49a7598dafdde614c5b07fc2e63a8ae6b36
Redas <redass95@outlook.com>
2022-10-21 22:47:43 +0300

    command design pattern, dunno ar geras

---
+++ Carmageddon/Command/ConcreteCommand.cs
@@ -0,0 +1,24 @@
+﻿using Carmageddon.Forms.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carmageddon.Forms.Command
+{
+    public class ConcreteCommand : Command
+    {
+        public override void Execute(Car car, Image image, Stack<Car> cars, Stack<Image> previousImages)
+        {
+            cars.Push(car);
+            previousImages.Push(image);
+        }
+
+        public override Image Undo(Stack<Car> cars, Stack<Image> previousImages)
+        {
+            cars.Pop();
+            return previousImages.Pop();
+        }
+    }
+}
---
+++ Carmageddon/Command/Invoker.cs
@@ -0,0 +1,40 @@
+﻿using Carmageddon.Forms.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carmageddon.Forms.Command
+{
+    public class Invoker
+    {
+        Receiver receiver = new Receiver();
+        Stack<Image> previousImages = new Stack<Image>();
+        Stack<Car> _cars = new Stack<Car>();
+        int count = 0;
+
+        public void AddCar(Car car, Image image)
+        {
+            Command command = new ConcreteCommand();
+            command.Execute(car, image, _cars, previousImages);
+            count++;
+            receiver.Action();
+        }
+        public Image Undo()
+        {
+           if(count > 0)
+            {
+                Command command = new ConcreteCommand();
+                count--;
+                return command.Undo(_cars, previousImages);
+            }
+           else
+                return null;
+        }
+        public Stack<Car> CarStack()
+        {
+            return _cars;
+        }
+    }
+}
